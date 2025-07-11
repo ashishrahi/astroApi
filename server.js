2015890c626c50e04457018fc500bc7db720c6c7
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-07-11 13:29:52 +0530

    temp: save local changes before revert

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import app from './app.js'
-import serverless from 'serverless-http';
 import dbConnect from './src/Config/dbConnect.js'
 
-export default serverless(async (req, res) => {
-  try {
-    return await app(req, res);
-  } catch (err) {
-    console.error('🔥 Uncaught handler error:', err);
-    res.status(500).send('Internal Server Error');
-  }
-});
 dbConnect()
 const Port = process.env.PORT
 
